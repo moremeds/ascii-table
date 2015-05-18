4d717fe75b130facdf743ee9b04408308586b74e
moremds <lcxxcllcx@gmail.com>
2015-05-18 21:34:35 +0100

    added a footer support

--- example/simple.js
+++ example/simple.js
@@ -1,1 +1,13 @@
+var AsciiTable = require('../ascii-table');
 
+var table = new AsciiTable('A Title');
+
+table
+    .setHeading('', 'Name', 'Age')
+    .addRow(1, 'Bob', 52)
+    .addRow(2, 'John', 34)
+    .addRow(3, 'Jim', 83);
+
+table.addFooter("123", "456");
+
+console.log(table.toString())
